5932f621dd31a030a85a57e03137e92fa351924c
HeronErin <HeronErin@protonmail.com>
2023-11-18 21:50:28 -0500

    Add gpl3 disclaimer

--- segment.py
+++ segment.py
@@ -1,4 +1,19 @@
 # Script for ttsing on the cloud and sending it back the the main server (as tortoise_tts is slow but SUPER high quality)
+
+
+
+# This program is free software; you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation; either 
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program;
+# if not, see http://www.gnu.org/licenses/gpl-3.0
+
+
 sc = "scripts/tortoise_tts.py"
 import os, time, concurrent.futures, requests, base64, random
 
